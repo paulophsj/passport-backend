6b29bb2549d92fdc037bbf092d3be02fd127d9b4
paulophsj <snoopyphs@gmail.com>
2026-01-27 12:57:59 -0300

    Criando busca personalizada para encontrar perfis com o ID do usuário

--- src/main/java/com/phsj/passport/model/acesso/PerfilRepository.java
+++ src/main/java/com/phsj/passport/model/acesso/PerfilRepository.java
@@ -1,6 +1,17 @@
 package com.phsj.passport.model.acesso;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+    @Query("""
+        SELECT p
+        FROM Usuario u
+        JOIN u.roles p
+        WHERE u.id = :userId
+    """)
+    List<Perfil> findAllByUserId(@Param("userId") Long userId);
 }
