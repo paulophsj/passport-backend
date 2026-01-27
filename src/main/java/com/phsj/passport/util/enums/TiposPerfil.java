75f73b58f8d0f4103e3950b815256ac41401602d
paulophsj <snoopyphs@gmail.com>
2026-01-27 12:52:09 -0300

    Criando ENUM de TiposPerfil para utilizar em Services e demais localidades

--- src/main/java/com/phsj/passport/util/enums/TiposPerfil.java
+++ src/main/java/com/phsj/passport/util/enums/TiposPerfil.java
@@ -1,4 +1,16 @@
 package com.phsj.passport.util.enums;
 
 public enum TiposPerfil {
+    CLIENTE("CLIENTE"),
+    ADMIN("ADMIN");
+
+    private final String tipo;
+
+    TiposPerfil(String tipo){
+        this.tipo = tipo;
+    };
+
+    public String getTipo(){
+        return tipo;
+    }
 }
